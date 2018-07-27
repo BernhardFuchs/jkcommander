a2d00ab23cc530ffd7b11b230356b4153625b4d6
Bernhard Fuchs <bernifox82@gmail.com>
2018-07-27 08:28:35 +0200

    omelette added

--- src/jkcommander.js
+++ src/jkcommander.js
@@ -1,5 +1,34 @@
 #! /usr/bin/env node
 
+const omelette = require('omelette');
+
+// Write your CLI template.
+const completion = omelette(`jkcommander|jk <action> <user> <repo>`);
+
+// Bind events for every template part.
+completion.on('action', ({ reply }) => {
+  reply([ 'initRepo', 'createBranchRemote', 'dummyScript' ])
+})
+
+completion.on('user', ({ reply }) => {
+  reply(fs.readdirSync('/Users/'))
+})
+
+completion.on('repo', ({ before, reply }) => {
+  reply([
+    `http://github.com/${before}/helloworld`,
+    `http://github.com/${before}/blabla`
+  ])
+})
+
+// Initialize the omelette.
+completion.init()
+
+// If you want to have a setup feature, you can use `omeletteInstance.setupShellInitFile()` function.
+if (~process.argv.indexOf('--setup')) {
+  completion.setupShellInitFile()
+}
+
 const jkcommander = require('commander');
 
 const initUpstreamRepository = require('./commands/init-upstream-repository');
