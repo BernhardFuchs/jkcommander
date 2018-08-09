6275c4b322b019425a0f350e1aee37ef025b3ae9
Bernhard Fuchs <bernifox82@gmail.com>
2018-08-09 21:47:09 +0200

    add ssh key script working

--- src/commands/ssh-add.js
+++ src/commands/ssh-add.js
@@ -1,9 +1,19 @@
 require('colors');
-const shell = require('shelljs');
+const exec = require('child_process').exec;
+const pathToKey = '~/.ssh/id_rsa';
 
 const sshAdd = () => {
-  shell.exec('ssh-agent bash');
-  shell.exec('ssh-add ~/.ssh/id_rsa');
+  console.log(`Adding ${pathToKey}`.bgGreen);
+  exec(`ssh-add ${pathToKey}`,
+    (error, stdout, stderr) => {
+      if (error !== null) {
+        console.log(`${error}`.red);
+        process.exit(1);
+      }
+      console.log(`${stdout}`.green);
+      console.log(`${stderr}`.bgRed);
+    });
+  
 }
 
 module.exports = sshAdd;
